b5a71fb4cb0bb42408607da68ed78704e1040595
Christoph Meier <chris_reiss@outlook.de>
2025-02-08 16:06:21 +0100

    Delete .history directory

--- .history/start_20250208143441.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem hallo
-powershell -Command "Start-Process powershell -ArgumentList '-NoExit', '-Command', 'python ./src/aurora.py' -WindowStyle Normal"
-exit
--- .history/start_20250208144756.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -Command "Start-Process powershell -ArgumentList '-NoExit', '-Command', 'python ./src/aurora.py' -WindowStyle Normal"
-exit
