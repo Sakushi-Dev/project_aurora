0d23ccc8f34bf875965d61e0fc43d67aa283ce55
Christoph Meier <chris_reiss@outlook.de>
2025-02-23 16:46:03 +0000

    refactor: streamline restart handling by replacing subprocess with os.system for script relaunch

--- src/commands.py
+++ src/commands.py
@@ -330,15 +330,13 @@ def handle_delete():
 
 def handle_restart():
     os.system('cls' if os.name == 'nt' else 'clear')  # Bildschirm leeren
-    
+
     # Alle Python-Prozesse beenden (Achtung: Beendet ALLE Python-Instanzen!)
     subprocess.run('taskkill /F /IM python.exe /T >nul 2>&1', shell=True)
     time.sleep(1)  # Warte 1 Sekunde
 
     # Skript neu starten (ohne Pfad-Ausgabe in PowerShell)
-    subprocess.Popen(f'python "{sys.argv[0]}" >nul 2>&1', shell=True)
-    
-    sys._exit(0)  # Beendet den aktuellen Prozess
+    os.system('python "./src/aurora.py"')
 
 
 def handle_reset():
